e1c3645371118e6e4183705c379e838922f300eb
JoyLubega <jollankent@gmail.com>
2019-05-23 13:22:38 +0300

    Add Form data

--- Mentorship/Mentorship/spiders/post_spider.py
+++ Mentorship/Mentorship/spiders/post_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 from scrapy import FormRequest
+from scrapy.utils.response import open_in_browser
 
 class PostSpider(scrapy.Spider):
     name = "post_spider"
@@ -9,32 +10,42 @@ class PostSpider(scrapy.Spider):
             'https://procesos.ramajudicial.gov.co/consultaprocesos/ConsultaJusticias21.aspx',
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse_cities)
+            yield scrapy.Request(url=url, dont_filter = True, callback=self.parse_cities)
 
     def parse_cities(self, response):
-        cities=response.xpath('//*[@id="ddlJuzgados"]/option/text()').extract()
-        cities.pop(0)
-        valid_cities = cities
-        for city in valid_cities:
-            # link = response.xpath("//input/@onclick[contains(., 'juzgados')]")
-            link='https://procesos.ramajudicial.gov.co/jepms/'+city.lower()+'jepms/conectar.asp'
-            yield scrapy.Request(url=link, callback= self.parse_form)
+
+        # get All cities
+        # cities=response.xpath('//*[@id="ddlJuzgados"]/option/text()').extract()
+        urls = response.xpath('//*[@id="ddlJuzgados"]/option/@value').extract()
+        urls.pop(0)
+        for url in urls:
+            
+            valid_url = 'https://procesos.ramajudicial.gov.co'+url
+
+            yield response.follow(url=valid_url, callback= self.parse_form)
 
     def parse_form(self, response):
         
+        form_data ={
+            "cbadju":"3",
+            "norad":"98703271"
+        }
         yield FormRequest.from_response(
             response, 
-            formdata={
-                "cbadju":"Identification document of the subject",
-                "norad":"98703271"
-            },
-            callback=self.parse_result
+            formdata=form_data,
+            method='POST',
+            callback=self.parse_result   
         )
-    def parse_result():
-        print("hey")
+    def parse_result(self, response):
+        number_radication = response.css('.timpar a::attr(href)').extract_first()
+        headers = response.xpath('//*[@class="e_tablas"]/th/text()').extract()
+        headers.pop(0)
+        row = response.xpath('//*[@class="timpar"]/td/text()').extract()
 
-           
-            
+        data = dict(zip(headers, row))
+        yield data
+        
+        
             
 
         
